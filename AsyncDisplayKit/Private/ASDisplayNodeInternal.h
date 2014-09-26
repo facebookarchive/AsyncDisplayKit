c51a58cd85502eb4843a2c978b478a419ed2daf3
Nadine Salter <nadi@fb.com>
2014-09-26 15:17:46 -0700

    Update ASDisplayNodeInternal.h.

--- AsyncDisplayKit/Private/ASDisplayNodeInternal.h
+++ AsyncDisplayKit/Private/ASDisplayNodeInternal.h
@@ -89,6 +89,7 @@ BOOL ASDisplayNodeSubclassOverridesSelector(Class subclass, SEL selector);
 
 // Swizzle to extend the builtin functionality with custom logic
 - (BOOL)__shouldLoadViewOrLayer;
+- (BOOL)__shouldSize;
 
 - (void)__layout;
 - (void)__setSupernode:(ASDisplayNode *)supernode;
