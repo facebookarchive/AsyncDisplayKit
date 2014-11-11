1f8b446b9c4e010421116a0d86da7ebe4349bc03
Huy Nguyen <allforone1511@gmail.com>
2014-11-11 13:52:11 +0200

    Add CollectionViewSample.
    Update sample for more items.

---
+++ examples/CollectionViewSample/CollectionViewSample/ViewController.m
@@ -0,0 +1,81 @@
+/* This file provided by Facebook is for non-commercial testing and evaluation
+ * purposes only.  Facebook reserves all rights not expressly granted.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * FACEBOOK BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+#import "ViewController.h"
+
+#import <AsyncDisplayKit/AsyncDisplayKit.h>
+
+@interface ViewController () <ASCollectionViewDataSource, ASCollectionViewDelegate>
+{
+  ASCollectionView *_collectionView;
+}
+
+@end
+
+
+@implementation ViewController
+
+#pragma mark -
+#pragma mark UIViewController.
+
+- (instancetype)init
+{
+  if (!(self = [super init]))
+    return nil;
+  
+  UICollectionViewFlowLayout *layout = [[UICollectionViewFlowLayout alloc] init];
+  layout.scrollDirection = UICollectionViewScrollDirectionHorizontal;
+  
+  _collectionView = [[ASCollectionView alloc] initWithFrame:CGRectZero collectionViewLayout:layout];
+  _collectionView.asyncDataSource = self;
+  _collectionView.asyncDelegate = self;
+  _collectionView.backgroundColor = [UIColor whiteColor];
+  
+  return self;
+}
+
+- (void)viewDidLoad
+{
+  [super viewDidLoad];
+  
+  [self.view addSubview:_collectionView];
+}
+
+- (void)viewWillLayoutSubviews
+{
+  _collectionView.frame = self.view.bounds;
+}
+
+- (BOOL)prefersStatusBarHidden
+{
+  return YES;
+}
+
+
+#pragma mark -
+#pragma mark ASCollectionView data source.
+
+- (ASCellNode *)collectionView:(ASCollectionView *)collectionView nodeForItemAtIndexPath:(NSIndexPath *)indexPath
+{
+  NSString *text = [NSString stringWithFormat:@"[%ld.%ld] says hi", indexPath.section, indexPath.item];
+  ASTextCellNode *node = [[ASTextCellNode alloc] init];
+  node.text = text;
+  node.backgroundColor = [UIColor lightGrayColor];
+  
+  return node;
+}
+
+- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section
+{
+  return 300;
+}
+
+@end
---
+++ examples/CollectionViewSample/CollectionViewSample/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CollectionViewSample
+//
+//  Created by Huy Nguyen on 11/11/14.
+//  Copyright (c) 2014 Facebook. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+  }
+}
