9cf71828ad696271568d0255138adf82383a9adc
Nadine Salter <rhyme@ndreason.com>
2015-01-24 12:04:06 -0800

    Include ASEditableTextNode in framework header.

--- AsyncDisplayKit/AsyncDisplayKit.h
+++ AsyncDisplayKit/AsyncDisplayKit.h
@@ -13,6 +13,8 @@
 #import <AsyncDisplayKit/ASImageNode.h>
 #import <AsyncDisplayKit/ASTextNode.h>
 
+#import <AsyncDisplayKit/ASEditableTextNode.h>
+
 #import <AsyncDisplayKit/ASBasicImageDownloader.h>
 #import <AsyncDisplayKit/ASMultiplexImageNode.h>
 #import <AsyncDisplayKit/ASNetworkImageNode.h>
