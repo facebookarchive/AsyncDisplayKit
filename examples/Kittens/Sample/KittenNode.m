6246f077fc9289e7bbb82fdbe8ed9f297c4190cb
Nadine Salter <nadi@fb.com>
2015-02-03 08:27:21 -0800

    Update comment in Kittens example project.

--- examples/Kittens/Sample/KittenNode.m
+++ examples/Kittens/Sample/KittenNode.m
@@ -73,7 +73,7 @@ static const CGFloat kInnerPadding = 10.0f;
 
   _kittenSize = size;
 
-  // kitten image, with a purple background colour serving as placeholder
+  // kitten image, with a solid background colour serving as placeholder
   _imageNode = [[ASNetworkImageNode alloc] init];
   _imageNode.backgroundColor = ASDisplayNodeDefaultPlaceholderColor();
   _imageNode.URL = [NSURL URLWithString:[NSString stringWithFormat:@"http://placekitten.com/%zd/%zd",
