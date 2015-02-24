0e15b061d013a63179428617a379e140b2ae8260
Li Tan <lit@fb.com>
2015-02-23 20:45:15 -0800

    Sort array before data fetching

--- AsyncDisplayKit/Details/ASDataController.mm
+++ AsyncDisplayKit/Details/ASDataController.mm
@@ -471,7 +471,7 @@ static void *kASSizingQueueContext = &kASSizingQueueContext;
 - (NSArray *)nodesAtIndexPaths:(NSArray *)indexPaths
 {
   ASDisplayNodeAssertMainThread();
-  return ASFindElementsInMultidimensionalArrayAtIndexPaths(_nodes, indexPaths);
+  return ASFindElementsInMultidimensionalArrayAtIndexPaths(_nodes, [indexPaths sortedArrayUsingSelector:@selector(compare:)]);
 }
 
 @end
