dd10fda42c5ed4da0e8ba61ea7e5600b802983d8
Nadine Salter <rhyme@ndreason.com>
2015-03-23 16:39:21 -0700

    Fix ASSnapshotTestCase #import.

--- AsyncDisplayKitTests/ASSnapshotTestCase.h
+++ AsyncDisplayKitTests/ASSnapshotTestCase.h
@@ -6,7 +6,7 @@
  * of patent rights can be found in the PATENTS file in the same directory.
  */
 
-#import "FBSnapshotTestCase.h"
+#import <FBSnapshotTestCase/FBSnapshotTestCase.h>
 
 #import <AsyncDisplayKit/ASDisplayNode.h>
 
