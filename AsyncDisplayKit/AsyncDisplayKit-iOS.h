b8f60e126a9c1ea0d8511def90daad5ad499d26a
Niels van Hoorn <nvh@nvh.io>
2015-05-11 18:50:56 +0200

    Added iOS framework target for Carthage support

---
+++ AsyncDisplayKit/AsyncDisplayKit-iOS.h
@@ -0,0 +1,16 @@
+/* Copyright (c) 2014-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ */
+#import <UIKit/UIKit.h>
+
+//! Project version number for AsyncDisplayKit-iOS.
+FOUNDATION_EXPORT double AsyncDisplayKitVersionNumber;
+
+//! Project version string for AsyncDisplayKit-iOS.
+FOUNDATION_EXPORT const unsigned char AsyncDisplayKitVersionString[];
+
+#import <AsyncDisplayKit/AsyncDisplayKit.h>
