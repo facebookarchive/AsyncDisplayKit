c70061886220a13cb5f590d9d54bbe491a8cc4ab
Scott Goodson <scottgoodson@gmail.com>
2015-06-21 21:04:09 -0700

    Improvements to thread safety during closely spaced edit events.
    This applies primarily when beginUpdates / endUpdates is not used.
    Due to user interaction-driven edits, like reloads or adding content
    at the bottom, sometimes this is unavoidable in app design and thus
    critical.
    
    I have a diff in flight to make ASDataController / ASRangeController
    robust against very aggressive thrash testing, which will be added
    both to the unit test suite and this new example project.

--- AsyncDisplayKit/ASCellNode.m
+++ AsyncDisplayKit/ASCellNode.m
@@ -110,6 +110,8 @@ static const CGFloat kFontSize = 18.0f;
 {
   CGSize availableSize = CGSizeMake(constrainedSize.width - 2 * kHorizontalPadding,
                                     constrainedSize.height - 2 * kVerticalPadding);
+  availableSize.width = MAX(0, availableSize.width);
+  availableSize.height = MAX(0, availableSize.height);
   CGSize textNodeSize = [_textNode measure:availableSize];
 
   return CGSizeMake(ceilf(2 * kHorizontalPadding + textNodeSize.width),
