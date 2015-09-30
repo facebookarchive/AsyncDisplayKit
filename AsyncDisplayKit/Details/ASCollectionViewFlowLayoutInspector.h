c9f93cde76d9a227a651c2150dc4fdba230e60a8
Levi McCallum <levi.mccallum@nfl.com>
2015-09-29 18:06:00 -0700

    Update copywright preamble

--- AsyncDisplayKit/Details/ASCollectionViewFlowLayoutInspector.h
+++ AsyncDisplayKit/Details/ASCollectionViewFlowLayoutInspector.h
@@ -1,10 +1,10 @@
-//
-//  ASCollectionViewFlowLayoutInspector.h
-//  Pods
-//
-//  Created by Levi McCallum on 9/29/15.
-//
-//
+/* Copyright (c) 2015-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ */
 
 #import <Foundation/Foundation.h>
 
