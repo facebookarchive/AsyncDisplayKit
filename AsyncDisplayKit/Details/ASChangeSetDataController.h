8745a478ec74d43a557f5673dc72c6607c9a4ddc
Huy Nguyen <allforone1511@gmail.com>
2015-10-25 19:33:34 +0200

    Add documentation to ASChangeSetDataController.

--- AsyncDisplayKit/Details/ASChangeSetDataController.h
+++ AsyncDisplayKit/Details/ASChangeSetDataController.h
@@ -9,7 +9,11 @@
 #import <AsyncDisplayKit/ASDataController.h>
 
 /**
- * Subclass of ASDataController that enqueues and sorts edit commands during batch updating (using _ASHierarchyChangeSet).
+ * @abstract Subclass of ASDataController that simulates ordering of operations in batch updates defined in UITableView and UICollectionView.
+ * 
+ * @discussion The ordering is achieved by using _ASHierarchyChangeSet to enqueue and sort operations.
+ * More information about the ordering and the index paths used for operations can be found here:
+ * https://developer.apple.com/library/ios/documentation/UserExperience/Conceptual/TableView_iPhone/ManageInsertDeleteRow/ManageInsertDeleteRow.html#//apple_ref/doc/uid/TP40007451-CH10-SW17
  *
  * @see ASDataController
  * @see _ASHierarchyChangeSet
