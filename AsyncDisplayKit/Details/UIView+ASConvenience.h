fd0b3aaf5224f776b63274e06fb26f300622a159
Huy Nguyen <allforone1511@gmail.com>
2015-10-26 06:29:01 +0200

    Revert "Revert "Make ASDisplayNode.name Thread Safe""
    This reverts commit 906d7c759b471eda67b95574199b6f91d46b6782.

--- AsyncDisplayKit/Details/UIView+ASConvenience.h
+++ AsyncDisplayKit/Details/UIView+ASConvenience.h
@@ -31,7 +31,6 @@
 @property (nonatomic, assign) CGFloat borderWidth;
 @property (nonatomic, assign, getter = isOpaque) BOOL opaque;
 @property (nonatomic, retain) __attribute__((NSObject)) CGColorRef borderColor;
-@property (nonatomic, copy) NSString *asyncdisplaykit_name;
 @property (nonatomic, retain) __attribute__((NSObject)) CGColorRef backgroundColor;
 @property (nonatomic, assign) BOOL allowsEdgeAntialiasing;
 @property (nonatomic, assign) unsigned int edgeAntialiasingMask;
@@ -78,8 +77,4 @@
 // Accessibility identification support
 @property (nonatomic, copy)          NSString *accessibilityIdentifier;
 
-@end
-
-@interface CALayer (ASDisplayNodeLayer)
-@property (atomic, copy) NSString *asyncdisplaykit_name;
 @end
