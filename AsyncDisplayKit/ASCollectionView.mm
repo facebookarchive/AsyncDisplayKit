24cb1d5cb0a795edabfec18b0c8e08ae3b5fde70
Levi McCallum <levi@levimccallum.com>
2015-11-29 21:49:34 -0800

    Fix broken build

--- AsyncDisplayKit/ASCollectionView.mm
+++ AsyncDisplayKit/ASCollectionView.mm
@@ -664,7 +664,7 @@ static BOOL _isInterceptedSelector(SEL sel)
 
 - (ASSizeRange)dataController:(ASDataController *)dataController constrainedSizeForNodeAtIndexPath:(NSIndexPath *)indexPath
 {
-  ASSizeRange constrainedSize = CGSizeZero;
+  ASSizeRange constrainedSize = kInvalidSizeRange;
   if (_layoutInspector) {
     constrainedSize = [_layoutInspector collectionView:self constrainedSizeForNodeAtIndexPath:indexPath];
   }
