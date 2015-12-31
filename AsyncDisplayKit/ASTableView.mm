50c97b263617a4f8694485f4169e9f11fb2304f1
appleguy <scottgoodson@gmail.com>
2015-12-30 23:42:18 -0800

    Revert "[ASCellNode] Forward pointInside to node implementation from UITableViewCell."

--- AsyncDisplayKit/ASTableView.mm
+++ AsyncDisplayKit/ASTableView.mm
@@ -76,11 +76,6 @@ static NSString * const kCellReuseIdentifier = @"_ASTableViewCell";
   _node.highlighted = highlighted;
 }
 
-- (BOOL)pointInside:(CGPoint)point withEvent:(UIEvent *)event
-{
-  return [_node pointInside:point withEvent:event];
-}
-
 @end
 
 #pragma mark -
