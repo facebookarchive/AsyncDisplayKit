d44193fb8d420bbe86711f9b0b142a7e6f93b4cc
Garrett Moon <garrett@pinterest.com>
2016-02-05 14:06:17 -0800

    ASMultiplexImageNode actually always calls with the main queue. There shouldn't be a reason to *not* provide a queue.

---
+++ AsyncDisplayKit/Details/ASPINRemoteImageDownloader.h
@@ -0,0 +1,13 @@
+//
+//  ASPINRemoteImageDownloader.h
+//  Pods
+//
+//  Created by Garrett Moon on 2/5/16.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ASPINRemoteImageDownloader : NSObject
+
+@end
---
+++ AsyncDisplayKit/Details/ASPINRemoteImageDownloader.m
@@ -0,0 +1,13 @@
+//
+//  ASPINRemoteImageDownloader.m
+//  Pods
+//
+//  Created by Garrett Moon on 2/5/16.
+//
+//
+
+#import "ASPINRemoteImageDownloader.h"
+
+@implementation ASPINRemoteImageDownloader
+
+@end
