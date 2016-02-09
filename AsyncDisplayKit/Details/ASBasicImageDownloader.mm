0be51c1ca2abfd04cb824db488e16ce21c550e44
Garrett Moon <garrett@pinterest.com>
2016-02-08 16:30:34 -0800

    Leave ASBasicImageDownloader alone.

--- AsyncDisplayKit/Details/ASBasicImageDownloader.mm
+++ AsyncDisplayKit/Details/ASBasicImageDownloader.mm
@@ -230,7 +230,7 @@ static const char *kContextKey = NSStringFromClass(ASBasicImageDownloaderContext
 
 - (id)downloadImageWithURL:(NSURL *)URL
              callbackQueue:(dispatch_queue_t)callbackQueue
-          downloadProgress:(void (^)(CGFloat))downloadProgressBlock
+     downloadProgressBlock:(void (^)(CGFloat))downloadProgressBlock
                 completion:(void (^)(CGImageRef, NSError *))completion
 {
   ASBasicImageDownloaderContext *context = [ASBasicImageDownloaderContext contextForURL:URL];
