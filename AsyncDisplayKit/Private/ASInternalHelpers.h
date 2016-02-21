ff59401b05a0a1b1090929c7cb552549835b24d4
Adlai Holler <guy.on.internet@icloud.com>
2016-02-21 11:23:54 -0800

    [ASInternalHelpers] Improve spacing

--- AsyncDisplayKit/Private/ASInternalHelpers.h
+++ AsyncDisplayKit/Private/ASInternalHelpers.h
@@ -51,9 +51,9 @@ ASDISPLAYNODE_INLINE void ASPerformBlockWithoutAnimation(BOOL withoutAnimation, 
 
 ASDISPLAYNODE_INLINE void ASBoundsAndPositionForFrame(CGRect rect, CGPoint origin, CGPoint anchorPoint, CGRect *bounds, CGPoint *position)
 {
-  *bounds      = (CGRect){ origin, rect.size };
-  *position    = CGPointMake(rect.origin.x + rect.size.width * anchorPoint.x,
-                            rect.origin.y + rect.size.height * anchorPoint.y);
+  *bounds   = (CGRect){ origin, rect.size };
+  *position = CGPointMake(rect.origin.x + rect.size.width * anchorPoint.x,
+                          rect.origin.y + rect.size.height * anchorPoint.y);
 }
 
 @interface NSIndexPath (ASInverseComparison)
