397181538e9e68b75a3e96559c530c8c8a31660d
Michael Schneider <mischneider1@gmail.com>
2016-03-05 20:09:31 -0800

    Change header of main.m

--- examples/ASViewController/Sample/main.m
+++ examples/ASViewController/Sample/main.m
@@ -1,10 +1,13 @@
-//
-//  main.m
-//  Sample
-//
-//  Created by Michael Schneider on 3/5/16.
-//  Copyright © 2016 AsyncDisplayKit. All rights reserved.
-//
+/* This file provided by Facebook is for non-commercial testing and evaluation
+ * purposes only.  Facebook reserves all rights not expressly granted.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * FACEBOOK BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
 
 #import <UIKit/UIKit.h>
 #import "AppDelegate.h"
