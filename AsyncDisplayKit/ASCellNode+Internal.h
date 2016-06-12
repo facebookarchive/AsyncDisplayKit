c857e809f41eea68eb78856a91dffe34aa8abaa2
Hannah Troisi <htroisi@gmail.com>
2016-06-11 23:31:39 -0700

    Clean up header comments (for consistent Facebook licensing info) (#1741)
    [Licensing] Clean up header comments (for consistent Facebook licensing info)

--- AsyncDisplayKit/ASCellNode+Internal.h
+++ AsyncDisplayKit/ASCellNode+Internal.h
@@ -1,9 +1,13 @@
 //
 //  ASCellNode+Internal.h
-//  Pods
+//  AsyncDisplayKit
 //
 //  Created by Max Gu on 2/19/16.
 //
+//  Copyright (c) 2014-present, Facebook, Inc.  All rights reserved.
+//  This source code is licensed under the BSD-style license found in the
+//  LICENSE file in the root directory of this source tree. An additional grant
+//  of patent rights can be found in the PATENTS file in the same directory.
 //
 
 #import "ASCellNode.h"
--- AsyncDisplayKit/ASPagerFlowLayout.h
+++ AsyncDisplayKit/ASPagerFlowLayout.h
@@ -3,7 +3,11 @@
 //  AsyncDisplayKit
 //
 //  Created by Levi McCallum on 2/12/16.
-//  Copyright © 2016 Facebook. All rights reserved.
+//
+//  Copyright (c) 2014-present, Facebook, Inc.  All rights reserved.
+//  This source code is licensed under the BSD-style license found in the
+//  LICENSE file in the root directory of this source tree. An additional grant
+//  of patent rights can be found in the PATENTS file in the same directory.
 //
 
 #import <UIKit/UIKit.h>
