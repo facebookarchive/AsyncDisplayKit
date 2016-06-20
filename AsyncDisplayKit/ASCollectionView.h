9873f4bd7df1341e215e818f7d6f3d4f46813509
Adlai Holler <adlai@pinterest.com>
2016-06-20 16:40:10 -0700

    Deprecate willDisplayNode:/didEndDisplayingNode: methods

--- AsyncDisplayKit/ASCollectionView.h
+++ AsyncDisplayKit/ASCollectionView.h
@@ -430,19 +430,6 @@ NS_ASSUME_NONNULL_BEGIN
 
 @optional
 
-- (void)collectionView:(ASCollectionView *)collectionView willDisplayNodeForItemAtIndexPath:(NSIndexPath *)indexPath;
-
-/**
- * Informs the delegate that the collection view did remove the provided node from the view hierarchy.
- * This may be caused by the node scrolling out of view, or by deleting the item
- * or its containing section with @c deleteItemsAtIndexPaths: or @c deleteSections: .
- * 
- * @param collectionView The sender.
- * @param node The node which was removed from the view hierarchy.
- * @param indexPath The index path at which the node was located before it was removed.
- */
-- (void)collectionView:(ASCollectionView *)collectionView didEndDisplayingNode:(ASCellNode *)node forItemAtIndexPath:(NSIndexPath *)indexPath;
-
 /**
  * Receive a message that the collectionView is near the end of its data set and more data should be fetched if 
  * necessary.
@@ -475,9 +462,42 @@ NS_ASSUME_NONNULL_BEGIN
  * Informs the delegate that the collection view did remove the node which was previously
  * at the given index path from the view hierarchy.
  *
- * This method is deprecated. Use @c collectionView:didEndDisplayingNode:forItemAtIndexPath: instead.
+ * @warning AsyncDisplayKit processes table view edits asynchronously. The index path
+ *   passed into this method may not correspond to the same item in your data source
+ *   if your data source has been updated since the last edit was processed.
+ *
+ * This method is deprecated. Use @c visibleStateDidChange: on the cell node instead.
  */
 - (void)collectionView:(ASCollectionView *)collectionView didEndDisplayingNodeForItemAtIndexPath:(NSIndexPath *)indexPath ASDISPLAYNODE_DEPRECATED;
+
+/**
+ * Informs the delegate that the collection view will add the node
+ * at the given index path to the view hierarchy.
+ *
+ * @warning AsyncDisplayKit processes collection view edits asynchronously. The index path
+ *   passed into this method may not correspond to the same item in your data source
+ *   if your data source has been updated since the last edit was processed.
+ *
+ * This method is deprecated. Use @c visibleStateDidChange: on the cell node instead.
+ */
+- (void)collectionView:(ASCollectionView *)collectionView willDisplayNodeForItemAtIndexPath:(NSIndexPath *)indexPath ASDISPLAYNODE_DEPRECATED;
+
+/**
+ * Informs the delegate that the collection view did remove the provided node from the view hierarchy.
+ * This may be caused by the node scrolling out of view, or by deleting the item
+ * or its containing section with @c deleteItemsAtIndexPaths: or @c deleteSections: .
+ *
+ * @param collectionView The sender.
+ * @param node The node which was removed from the view hierarchy.
+ * @param indexPath The index path at which the node was located before it was removed.
+ *
+ * @warning AsyncDisplayKit processes collection view edits asynchronously. The index path
+ *   passed into this method may not correspond to the same item in your data source
+ *   if your data source has been updated since the last edit was processed.
+ *
+ * This method is deprecated. Use @c visibleStateDidChange: on the cell node instead.
+ */
+- (void)collectionView:(ASCollectionView *)collectionView didEndDisplayingNode:(ASCellNode *)node forItemAtIndexPath:(NSIndexPath *)indexPath ASDISPLAYNODE_DEPRECATED;
 
 @end
 
