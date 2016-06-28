a80d0dc426c0c4689481ad1d78a5235b5e324b55
Adlai Holler <adlai@pinterest.com>
2016-06-28 11:42:30 -0700

    [ASCollectionView] Remove unused header

--- AsyncDisplayKit/ASCollectionView.mm
+++ AsyncDisplayKit/ASCollectionView.mm
@@ -24,7 +24,6 @@
 #import "UICollectionViewLayout+ASConvenience.h"
 #import "ASRangeControllerUpdateRangeProtocol+Beta.h"
 #import "_ASDisplayLayer.h"
-#import "ASAvailability.h"
 
 static const NSUInteger kASCollectionViewAnimationNone = UITableViewRowAnimationNone;
 static const ASSizeRange kInvalidSizeRange = {CGSizeZero, CGSizeZero};
