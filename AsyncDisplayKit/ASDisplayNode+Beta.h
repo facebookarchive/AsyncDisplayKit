bd0254f30648d98cb0322fdd88f487e577be3f5b
Adlai Holler <adlai@pinterest.com>
2016-07-14 11:26:18 -0700

    [ASRangeControllerUpdateRangeProtocol] BREAKING Remove +setRangeModeForMemoryWarnings:, replace with global method

--- AsyncDisplayKit/ASDisplayNode+Beta.h
+++ AsyncDisplayKit/ASDisplayNode+Beta.h
@@ -9,6 +9,7 @@
 //
 
 #import "ASContextTransitioning.h"
+#import "ASLayoutRangeType.h"
 
 NS_ASSUME_NONNULL_BEGIN
 
@@ -115,6 +116,15 @@ ASDISPLAYNODE_EXTERN_C_END
  * progressImage block.
  */
 - (void)hierarchyDisplayDidFinish;
+
+/**
+ * Only ASLayoutRangeModeVisibleOnly or ASLayoutRangeModeLowMemory are recommended.  Default is ASLayoutRangeModeVisibleOnly,
+ * because this is the only way to ensure an application will not have blank / flashing views as the user navigates back after
+ * a memory warning.  Apps that wish to use the more effective / aggressive ASLayoutRangeModeLowMemory may need to take steps
+ * to mitigate this behavior, including: restoring a larger range mode to the next controller before the user navigates there,
+ * enabling .neverShowPlaceholders on ASCellNodes so that the navigation operation is blocked on redisplay completing, etc.
+ */
++ (void)setRangeModeForMemoryWarnings:(ASLayoutRangeMode)rangeMode;
 
 @end
 
