90cd7c8b263fc928b4dd0779bc235e3696cc5340
Hannah Trosi <htroisi@gmail.com>
2016-08-19 19:46:57 -0700

    Add property attributes

--- AsyncDisplayKit/ASImageNode.mm
+++ AsyncDisplayKit/ASImageNode.mm
@@ -50,9 +50,9 @@ struct ASImageNodeDrawParameters {
 @interface ASImageNodeContentsKey : NSObject {}
 
 @property (nonatomic, strong) UIImage *image;
-@property CGSize backingSize;
-@property CGRect imageDrawRect;
-@property BOOL isOpaque;
+@property (nonatomic, assign) CGSize backingSize;
+@property (nonatomic, assign) CGRect imageDrawRect;
+@property (nonatomic, assign) BOOL isOpaque;
 @property (nonatomic, strong) UIColor *backgroundColor;
 @property ASDisplayNodeContextModifier preContextBlock;
 @property ASDisplayNodeContextModifier postContextBlock;
