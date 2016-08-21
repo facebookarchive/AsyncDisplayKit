a38f3db6b0567e4e8001fbea812ede63d6858d6e
Yue-Wang-Google <wangyue@google.com>
2016-08-21 13:06:21 -0700

    set the right attributes for block properties. (#2113)
    * set the right attributes for block properties.
    
    * oops. should remove strong.
    
    * Update ASImageNode.mm
    
    * revert to use copy for blocks. See apple documentation below
    
    https://developer.apple.com/library/mac/documentation/Cocoa/Conceptual/ProgrammingWithObjectiveC/WorkingwithBlocks/WorkingwithBlocks.html#//apple_ref/doc/uid/TP40011210-CH8-SW12
    
    * make the block properties nonatomic.

--- AsyncDisplayKit/ASImageNode.mm
+++ AsyncDisplayKit/ASImageNode.mm
@@ -54,9 +54,9 @@ struct ASImageNodeDrawParameters {
 @property CGRect imageDrawRect;
 @property BOOL isOpaque;
 @property (nonatomic, strong) UIColor *backgroundColor;
-@property ASDisplayNodeContextModifier preContextBlock;
-@property ASDisplayNodeContextModifier postContextBlock;
-@property asimagenode_modification_block_t imageModificationBlock;
+@property (nonatomic, copy) ASDisplayNodeContextModifier preContextBlock;
+@property (nonatomic, copy) ASDisplayNodeContextModifier postContextBlock;
+@property (nonatomic, copy) asimagenode_modification_block_t imageModificationBlock;
 
 @end
 
