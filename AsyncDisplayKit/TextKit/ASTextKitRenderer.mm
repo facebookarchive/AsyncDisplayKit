0aae54e5aface081a8841083b4836990908b7bad
Adlai Holler <guy.on.internet@icloud.com>
2016-08-24 19:56:37 -0700

    [ASTextKitRenderer] Shift TextKit teardown onto the deallocation queue

--- AsyncDisplayKit/TextKit/ASTextKitRenderer.mm
+++ AsyncDisplayKit/TextKit/ASTextKitRenderer.mm
@@ -129,9 +129,17 @@ static NSCharacterSet *_defaultAvoidTruncationCharacterSet()
     // truncater do it's job again for the new constrained size. This is necessary as after a truncation did happen
     // the context would use the truncated string and not the original string to truncate based on the new
     // constrained size
+    __block ASTextKitContext *ctx = _context;
+    __block ASTextKitTailTruncater *tru = _truncater;
+    __block ASTextKitFontSizeAdjuster *adj = _fontSizeAdjuster;
     _context = nil;
     _truncater = nil;
     _fontSizeAdjuster = nil;
+    ASPerformBlockOnDeallocationQueue(^{
+      ctx = nil;
+      tru = nil;
+      adj = nil;
+    });
   }
 }
 
