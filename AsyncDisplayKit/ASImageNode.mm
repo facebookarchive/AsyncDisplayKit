07853a9327debcfd5fdc4cbba997ce2557484e0e
Adlai Holler <guy.on.internet@icloud.com>
2016-09-14 21:18:56 -0700

    Use blend mode copy in a couple more places

--- AsyncDisplayKit/ASImageNode.mm
+++ AsyncDisplayKit/ASImageNode.mm
@@ -677,7 +677,7 @@ extern asimagenode_modification_block_t ASImageNodeRoundBorderModificationBlock(
     [roundOutline addClip];
 
     // Draw the original image
-    [originalImage drawAtPoint:CGPointZero];
+    [originalImage drawAtPoint:CGPointZero blendMode:kCGBlendModeCopy alpha:1];
 
     // Draw a border on top.
     if (borderWidth > 0.0) {
@@ -700,7 +700,7 @@ extern asimagenode_modification_block_t ASImageNodeTintColorModificationBlock(UI
     // Set color and render template
     [color setFill];
     UIImage *templateImage = [originalImage imageWithRenderingMode:UIImageRenderingModeAlwaysTemplate];
-    [templateImage drawAtPoint:CGPointZero];
+    [templateImage drawAtPoint:CGPointZero blendMode:kCGBlendModeCopy alpha:1];
     
     UIImage *modifiedImage = UIGraphicsGetImageFromCurrentImageContext();
     UIGraphicsEndImageContext();
