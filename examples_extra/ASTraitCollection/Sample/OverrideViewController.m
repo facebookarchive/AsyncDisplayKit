43d8d584ea2dda64dac1709da19f72c7dbec38a3
ricky <rickycancro@gmail.com>
2016-09-26 12:28:39 -0700

    [ASTextKitFontSizeAdjuster] Use the constrainedSize’s height to adjust font scaling
    Previously we used `maximumNumberOfLines` and the longest word in a string to scale font size. This adds taking the `constrainedSize`’s height into consideration. If, without scaling, a string’s height is larger than the `constrainedSize` height, apply scaling.

--- examples_extra/ASTraitCollection/Sample/OverrideViewController.m
+++ examples_extra/ASTraitCollection/Sample/OverrideViewController.m
@@ -33,8 +33,8 @@ static NSString *kLinkAttributeName = @"PlaceKittenNodeLinkAttributeName";
     return nil;
   
   _textNode = [[ASTextNode alloc] init];
-  _textNode.flexGrow = YES;
-  _textNode.flexShrink = YES;
+  _textNode.style.flexGrow = YES;
+  _textNode.style.flexShrink = YES;
   _textNode.maximumNumberOfLines = 3;
   [self addSubnode:_textNode];
   
