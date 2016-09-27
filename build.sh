57456da67a04d01f1f49765d5b288278b75562be
Scott Goodson <scottgoodson@gmail.com>
2016-09-26 17:12:10 -0700

    Update build script to run against iOS 10 to fix Jenkins

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # **** Update me when new Xcode versions are released! ****
-PLATFORM="platform=iOS Simulator,OS=9.3,name=iPhone 6"
+PLATFORM="platform=iOS Simulator,OS=10.0,name=iPhone 7"
 SDK="iphonesimulator10.0"
 
 
