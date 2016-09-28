2691c7494e5fba8911f66a58001e7aa31f6648f2
Garrett Moon <garrett@excitedpixel.com>
2016-09-28 14:40:05 -0700

    @return is recognized by the syntax highlighter. (#2313)

--- AsyncDisplayKit/ASPagerNode.h
+++ AsyncDisplayKit/ASPagerNode.h
@@ -25,7 +25,7 @@ NS_ASSUME_NONNULL_BEGIN
  * This method replaces -collectionView:numberOfItemsInSection:
  *
  * @param pagerNode The sender.
- * @returns The total number of pages that can display in the pagerNode.
+ * @return The total number of pages that can display in the pagerNode.
  */
 - (NSInteger)numberOfPagesInPagerNode:(ASPagerNode *)pagerNode;
 
@@ -36,7 +36,7 @@ NS_ASSUME_NONNULL_BEGIN
  *
  * @param pagerNode The sender.
  * @param index     The index of the requested node.
- * @returns a node for display at this index. This will be called on the main thread and should
+ * @return a node for display at this index. This will be called on the main thread and should
  *   not implement reuse (it will be called once per row).  Unlike UICollectionView's version,
  *   this method is not called when the row is about to display.
  */
@@ -48,7 +48,7 @@ NS_ASSUME_NONNULL_BEGIN
  *
  * @param pagerNode The sender.
  * @param index     The index of the requested node.
- * @returns a block that creates the node for display at this index.
+ * @return a block that creates the node for display at this index.
  *   Must be thread-safe (can be called on the main thread or a background
  *   queue) and should not implement reuse (it will be called once per row).
  */
@@ -65,7 +65,7 @@ NS_ASSUME_NONNULL_BEGIN
  *
  * @param pagerNode The sender.
  * @param index The index of the node.
- * @returns A constrained size range for layout the node at this index.
+ * @return A constrained size range for layout the node at this index.
  */
 - (ASSizeRange)pagerNode:(ASPagerNode *)pagerNode constrainedSizeForNodeAtIndex:(NSInteger)index;
 
