adcb75f68b584ddb2a93452d3327896ee1adc28b
Scott Goodson <scottgoodson@gmail.com>
2016-10-03 22:23:30 -0700

    [ASDisplayNode] Change _pendingDisplayNodes from NSMutableSet to ASWeakSet.
    In at least some cases, _pendingDisplayNodes referencing self can cause the
    object to live longer than the application needs, while tearing down views.
    
    It also appears there may be a cancellation condition where this state is
    not balanced properly, which can cause a reference-cycle leak.  This change
    safely fixes this problem when tested against a repro case for it.
    
    I'm filing a followup task to make sure all cancellation edge cases have
    proper accounting for this codepath, even though the leak is fixed.

--- AsyncDisplayKit/ASDisplayNode.mm
+++ AsyncDisplayKit/ASDisplayNode.mm
@@ -2132,7 +2132,7 @@ static NSInteger incrementIfFound(NSInteger i) {
   ASDisplayNodeAssertMainThread();
 
   if (!_pendingDisplayNodes) {
-    _pendingDisplayNodes = [[NSMutableSet alloc] init];
+    _pendingDisplayNodes = [[ASWeakSet alloc] init];
   }
 
   [_pendingDisplayNodes addObject:node];
