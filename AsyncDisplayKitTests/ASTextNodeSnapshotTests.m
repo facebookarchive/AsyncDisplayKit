144d40811cd5994cc795114cb130e03d78e10c6f
Adlai Holler <adlai@pinterest.com>
2016-10-17 16:23:12 -0700

    Add some more truncation tests, using slow path as reference image

--- AsyncDisplayKitTests/ASTextNodeSnapshotTests.m
+++ AsyncDisplayKitTests/ASTextNodeSnapshotTests.m
@@ -100,4 +100,16 @@
   ASSnapshotVerifyNode(textNode, nil);
 }
 
+- (void)testThatSlowPathTruncationWorks
+{
+  self.recordMode = YES;
+  ASTextNode *textNode = [[ASTextNode alloc] init];
+  textNode.attributedText = [[NSAttributedString alloc] initWithString:@"judar judar judar judar judar judar"
+                                                            attributes:@{ NSFontAttributeName : [UIFont systemFontOfSize:30] }];
+  [textNode layoutThatFits:ASSizeRangeMake(CGSizeZero, CGSizeMake(100, 50))];
+  textNode.exclusionPaths = @[ [UIBezierPath bezierPath] ];
+  textNode.maximumNumberOfLines = 1;
+  ASSnapshotVerifyNode(textNode, nil);
+}
+
 @end
