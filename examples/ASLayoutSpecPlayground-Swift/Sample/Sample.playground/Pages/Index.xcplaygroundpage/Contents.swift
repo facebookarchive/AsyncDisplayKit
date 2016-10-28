8d1d47520c9307a53a198568004daa4e9ba979aa
George A <george@guidepal.com>
2016-10-28 11:04:04 +0200

    Added solutions to common issues.

--- examples/ASLayoutSpecPlayground-Swift/Sample/Sample.playground/Pages/Index.xcplaygroundpage/Contents.swift
+++ examples/ASLayoutSpecPlayground-Swift/Sample/Sample.playground/Pages/Index.xcplaygroundpage/Contents.swift
@@ -17,4 +17,8 @@ Tips:
     - View → Assistant Editor → Show Assistant Editor, to see the preview
  1. Make sure that **Timeline** as the element selected in the Assistant Editor
  1. You might have to click on stop/start (the one at the bottom of the screen, under the editor) a few times in case the timeline isn't updating.
+ - - -
+Solutions to Common Issues:
+ 1. If you're getting errors regarding **import Sample_Sources**, simply restart Xcode and try again.
+ 1. If you're getting issues with threading, restart the test until it works.
 */
