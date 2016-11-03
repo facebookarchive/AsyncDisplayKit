593e9cc5daee9fa77d01113ff795530024631073
Adlai Holler <adlai@pinterest.com>
2016-11-03 10:18:40 -0700

    Update call pattern test

--- AsyncDisplayKitTests/ASTableViewTests.m
+++ AsyncDisplayKitTests/ASTableViewTests.m
@@ -592,8 +592,10 @@
   // Assert that the beginning of the call pattern is correct.
   // There is currently noise that comes after that we will allow for this test.
   NSArray *expectedSelectors = @[ NSStringFromSelector(@selector(reloadData)),
+                                  NSStringFromSelector(@selector(beginUpdates)),
                                   NSStringFromSelector(@selector(insertSections:withRowAnimation:)),
-                                  NSStringFromSelector(@selector(insertRowsAtIndexPaths:withRowAnimation:))];
+                                  NSStringFromSelector(@selector(insertRowsAtIndexPaths:withRowAnimation:)),
+                                  NSStringFromSelector(@selector(endUpdates))];
   NSArray *firstSelectors = [selectors subarrayWithRange:NSMakeRange(0, expectedSelectors.count)];
   XCTAssertEqualObjects(firstSelectors, expectedSelectors);
 
@@ -627,8 +629,8 @@
                                  NSStringFromSelector(@selector(beginUpdates)),
                                  NSStringFromSelector(@selector(deleteSections:withRowAnimation:)),
                                  NSStringFromSelector(@selector(insertSections:withRowAnimation:)),
-                                 NSStringFromSelector(@selector(endUpdates)),
-                                 NSStringFromSelector(@selector(insertRowsAtIndexPaths:withRowAnimation:))];
+                                 NSStringFromSelector(@selector(insertRowsAtIndexPaths:withRowAnimation:)),
+                                 NSStringFromSelector(@selector(endUpdates))];
   NSArray *firstSelectors = [selectors subarrayWithRange:NSMakeRange(0, expectedSelectors.count)];
   XCTAssertEqualObjects(firstSelectors, expectedSelectors);
 
