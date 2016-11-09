19a262bd73d0da42ee091b81063e0e84a82c6e18
Garrett Moon <garrett@pinterest.com>
2016-11-08 16:12:28 -0800

    Disable for now, it's crashing in production.

--- AsyncDisplayKit/TextKit/ASTextKitRenderer.mm
+++ AsyncDisplayKit/TextKit/ASTextKitRenderer.mm
@@ -205,12 +205,14 @@ static NSCharacterSet *_defaultAvoidTruncationCharacterSet()
 
 - (BOOL)canUseFastPath
 {
-  return self.isScaled == NO
-    && self.usesCustomTruncation == NO
-    && self.usesExclusionPaths == NO
-    // NSAttributedString drawing methods ignore usesLineFragmentOrigin if max line count 1,
-    // rendering them useless:
-    && (_attributes.maximumNumberOfLines != 1 || isinf(_constrainedSize.width));
+  return NO;
+//Disable fast path for now as it's crashing in production
+//  return self.isScaled == NO
+//    && self.usesCustomTruncation == NO
+//    && self.usesExclusionPaths == NO
+//    // NSAttributedString drawing methods ignore usesLineFragmentOrigin if max line count 1,
+//    // rendering them useless:
+//    && (_attributes.maximumNumberOfLines != 1 || isinf(_constrainedSize.width));
 }
 
 #pragma mark - Drawing
