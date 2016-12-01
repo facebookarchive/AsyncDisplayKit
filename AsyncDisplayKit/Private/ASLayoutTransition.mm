66acd69e22f99eea3d56f27b48e3820a88d8a06e
Michael Schneider <mischneider1@gmail.com>
2016-12-01 07:03:39 -0800

    Better error message if a display node is created in layoutSpecThatFits: and deallocated before it was added to a subnode

--- AsyncDisplayKit/Private/ASLayoutTransition.mm
+++ AsyncDisplayKit/Private/ASLayoutTransition.mm
@@ -233,7 +233,7 @@ static inline std::vector<NSUInteger> findNodesInLayoutAtIndexesWithFilteredNode
     if (idx > lastIndex) { break; }
     if (idx >= firstIndex && [indexes containsIndex:idx]) {
       ASDisplayNode *node = (ASDisplayNode *)sublayout.layoutElement;
-      ASDisplayNodeCAssert(node, @"A flattened layout must consist exclusively of node sublayouts");
+      ASDisplayNodeCAssert(node, @"ASDisplayNode was deallocated before it was added to a subnode. It's likely the case that you use automatically manages subnodes and allocate a ASDisplayNode in layoutSpecThatFits: and don't have any strong reference to it.");
       // Ignore the odd case in which a non-node sublayout is accessed and the type cast fails
       if (node != nil) {
         BOOL notFiltered = (filteredNodes == nil || [filteredNodes indexOfObjectIdenticalTo:node] == NSNotFound);
