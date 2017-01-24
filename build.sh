a2cd79d40659202a8d0f32e1d2abb182826fe486
Michael Schneider <maicki@users.noreply.github.com>
2017-01-23 16:33:07 -0800

    Update build.sh script to use iOS 10.2 simulator (#2921)
    * Update build.sh script to use iOS 10.2 simulator
    
    * Remove the OS version all together
    
    * Remove comment

--- build.sh
+++ build.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-# **** Update me when new Xcode versions are released! ****
-PLATFORM="platform=iOS Simulator,OS=10.1,name=iPhone 7"
-SDK="iphonesimulator10.1"
+PLATFORM="platform=iOS Simulator,name=iPhone 7"
+SDK="iphonesimulator"
 
 
 # It is pitch black.
