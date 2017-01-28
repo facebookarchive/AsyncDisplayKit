f9778d8fc66309b97964e39ead806c2faa4dbeb1
Adlai Holler <adlai@pinterest.com>
2017-01-27 16:45:46 -0800

    Cleanup minor things

--- AsyncDisplayKit/Details/ASCollectionInternal.h
+++ AsyncDisplayKit/Details/ASCollectionInternal.h
@@ -15,7 +15,6 @@
 NS_ASSUME_NONNULL_BEGIN
 
 @protocol ASCollectionViewLayoutFacilitatorProtocol;
-@protocol ASListAdapter;
 @class ASCollectionNode;
 @class ASDataController;
 @class ASRangeController;
