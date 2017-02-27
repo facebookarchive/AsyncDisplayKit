ca8f867b437c950bf81b5df15a55d3fe24405cbd
Adlai Holler <adlai@pinterest.com>
2017-02-27 14:36:17 -0800

    Remove unrelated change

--- Source/Details/ASPhotosFrameworkImageRequest.h
+++ Source/Details/ASPhotosFrameworkImageRequest.h
@@ -60,6 +60,11 @@ extern NSString *const ASPhotosURLScheme;
  */
 @property (nonatomic, readonly) NSURL *url;
 
+/**
+ @return `YES` if `object` is an equivalent image request, `NO` otherwise.
+ */
+- (BOOL)isEqual:(id)object;
+
 @end
 
 NS_ASSUME_NONNULL_END
