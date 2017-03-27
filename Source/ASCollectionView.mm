a0090f39e0d32bea529ee074aade2ebab6616614
Huy Nguyen <huy@pinterest.com>
2017-03-27 13:21:12 +0100

    ASDataController can bail early if a change set is empty

--- Source/ASCollectionView.mm
+++ Source/ASCollectionView.mm
@@ -793,9 +793,11 @@ static NSString * const kReuseIdentifier = @"_ASCollectionReuseIdentifier";
   [_changeSet addCompletionHandler:completion];
   
   if (_batchUpdateCount == 0) {
-    _changeSet.animated = animated;
-    [_dataController updateWithChangeSet:_changeSet];
+    _ASHierarchyChangeSet *changeSet = _changeSet;
+    // Nil out _changeSet before forwarding to _dataController to allow the change set to cause subsequent batch updates on the same run loop
     _changeSet = nil;
+    changeSet.animated = animated;
+    [_dataController updateWithChangeSet:changeSet];
   }
 }
 
