f12224e43e6dadd21f4bebbffb97ad110df88281
Garrett Moon <garrett@pinterest.com>
2017-04-05 15:59:04 -0700

    Fix warnings

--- Source/Details/ASPINRemoteImageDownloader.m
+++ Source/Details/ASPINRemoteImageDownloader.m
@@ -276,15 +276,15 @@ static ASPINRemoteImageDownloader *sharedDownloader = nil;
   PINRemoteImageManagerPriority pi_priority = PINRemoteImageManagerPriorityMedium;
   switch (priority) {
     case ASImageDownloaderPriorityPreload:
-      pi_priority = PINRemoteImageManagerPriorityMedium;
+      pi_priority = PINRemoteImageManagerPriorityLow;
       break;
 
     case ASImageDownloaderPriorityImminent:
-      pi_priority = PINRemoteImageManagerPriorityHigh;
+      pi_priority = PINRemoteImageManagerPriorityDefault;
       break;
 
     case ASImageDownloaderPriorityVisible:
-      pi_priority = PINRemoteImageManagerPriorityVeryHigh;
+      pi_priority = PINRemoteImageManagerPriorityHigh;
       break;
   }
   [[self sharedPINRemoteImageManager] setPriority:pi_priority ofTaskWithUUID:downloadIdentifier];
